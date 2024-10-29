5a567f2ee45ef1c319b959b281239d53dc83abba
Steven Itti Leon <154503819+stevenittileon@users.noreply.github.com>
2024-10-29 17:14:30 +0530

    Add files via upload

---
+++ LeetCode/Hard/trappingwater.py
@@ -0,0 +1,20 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0 # checks if nothing is there
+
+        l,r = 0,len(height)-1
+        lmax,rmax = height[l],height[r]
+        res = 0
+        while l<r:# loops till both the pointers meet
+            if lmax < rmax:
+                l+=1
+                lmax = max(lmax,height[l]) ## if this line is exchanged with the one below, we must account for negative values
+                res += lmax - height[l]
+            else:
+                r-=1
+                rmax = max(rmax,height[r])## same for this line to account for negative values
+                res += rmax - height[r]
+        return res
+
+#uses two pointers for this technique to trap water
