1dcf3f722e8c041ee682b1466f1df362af9d8ab0
Steven Itti Leon <154503819+stevenittileon@users.noreply.github.com>
2025-02-11 21:10:33 +0530

    Add files via upload

---
+++ LeetCode/Medium/lowest_common_ancestor_BST.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+ class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || p==null || q == null)
+        {
+            return null;
+        }
+        if(Math.max(p.val,q.val)<root.val)
+        {
+            return lowestCommonAncestor(root.left,p,q);
+        }
+        if(Math.min(p.val,q.val)>root.val)
+        {
+            return lowestCommonAncestor(root.right,p,q);
+        }
+        else
+        {
+            return root;
+        }
+    }
+}
+/*
+ * 
+ */
