78c910c08fcfd784bb6cbab40fe3ff9198d6753c
Steven Itti Leon <154503819+stevenittileon@users.noreply.github.com>
2025-03-24 10:50:10 +0530

    Add files via upload

---
+++ GeeksForGeeks/find_kth_rotation.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class Solution {
+    public int findKRotation(List<Integer> arr) {
+        // Code here
+        int n = arr.size();
+        int min = Collections.min(arr);
+        int k = 0;
+        for(int i = 0; i<n;i++)
+        {
+            if(arr.get(i) == min)
+            {
+                return k;
+            }
+            else
+            {
+                k++;
+            }
+        }
+        return k;
+    }
+}
+/*
+Find Kth Rotation
+
+Given an increasing sorted rotated array arr of distinct integers. The array is right-rotated k times. 
+Find the value of k.
+Let's suppose we have an array arr = [2, 4, 6, 9], so if we rotate it by 2 times so that it will look like this:
+After 1st Rotation : [9, 2, 4, 6]
+After 2nd Rotation : [6, 9, 2, 4]
+
+Examples:
+
+Input: arr = [5, 1, 2, 3, 4]
+Output: 1
+
+Explanation: The given array is 5 1 2 3 4. The original sorted array is 1 2 3 4 5. 
+We can see that the array was rotated 1 times to the right.
+
+Input: arr = [1, 2, 3, 4, 5]
+Output: 0
+
+Explanation: The given array is not rotated. 
+*/
