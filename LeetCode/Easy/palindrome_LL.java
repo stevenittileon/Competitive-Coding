5f9f7a663246a824352d724ca51f9ce6677c8350
Steven Itti Leon <154503819+stevenittileon@users.noreply.github.com>
2025-03-31 00:38:40 +0530

    Add files via upload

---
+++ LeetCode/Easy/palindrome_LL.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverse(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
+        while(curr != null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head, fast = head.next;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode rev = reverse(slow.next);
+        slow.next = null;
+        while(rev != null){
+            if(head.val != rev.val){
+                return false;
+            }
+            head = head.next;
+            rev = rev.next;
+        }
+        return true;
+    }
+}
+/*
+234. Palindrome Linked List
+
+Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+
+Example 1:
+Input: head = [1,2,2,1]
+Output: true
+
+Example 2:
+Input: head = [1,2]
+Output: false 
+*/
