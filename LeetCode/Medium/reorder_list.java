8d1dfa72f68eb5ca07aa65be8e6f5fb74606fec1
Steven Itti Leon <154503819+stevenittileon@users.noreply.github.com>
2025-05-27 08:37:16 +0530

    Add files via upload

---
+++ LeetCode/Medium/reorder_list.java
@@ -0,0 +1,74 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if(head == null || head.next == null || head.next.next == null){
+            return;
+        }
+
+        ListNode slow = head, fast = head, prev = null;
+        while(fast != null && fast.next != null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;
+
+        ListNode second = reverse(slow);
+
+        merge(head, second);
+    }
+
+    private ListNode reverse(ListNode head){
+        ListNode prev = null, curr = head, next;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    private void merge(ListNode first, ListNode second){
+        while(first!= null && second != null){
+            ListNode temp1 = first.next;
+            ListNode temp2 = second.next;
+
+            first.next = second;
+            if(temp1 == null){
+                break;
+            }
+            second.next = temp1;
+
+            first = temp1;
+            second = temp2;
+        }
+    }
+}
+/*143. Reorder List
+
+You are given the head of a singly linked-list. The list can be represented as:
+
+L0 → L1 → … → Ln - 1 → Ln
+Reorder the list to be on the following form:
+
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+Example 1:
+Input: head = [1,2,3,4]
+Output: [1,4,2,3]
+
+Example 2:
+Input: head = [1,2,3,4,5]
+Output: [1,5,2,4,3] 
+*/
