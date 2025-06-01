251a211c765c5ffcba9f49ff134bbedc453dc79d
Steven Itti Leon <154503819+stevenittileon@users.noreply.github.com>
2025-06-01 23:07:27 +0530

    Add files via upload

---
+++ LeetCode/Easy/isomorphic_strings.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        map1 = []
+        map2 = []
+        for x in s:
+            map1.append(s.index(x))
+        for x in t:
+            map2.append(t.index(x))
+        if map1 == map2:
+            return True
+        return False
+'''205. Isomorphic Strings
+
+Given two strings s and t, determine if they are isomorphic.
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+All occurrences of a character must be replaced with another character while preserving the order of characters. 
+No two characters may map to the same character, but a character may map to itself.
+
+Example 1:
+Input: s = "egg", t = "add"
+Output: true
+
+Explanation:
+The strings s and t can be made identical by:
+Mapping 'e' to 'a'.
+Mapping 'g' to 'd'.
+
+Example 2:
+Input: s = "foo", t = "bar"
+Output: false
+
+Explanation:
+The strings s and t can not be made identical as 'o' needs to be mapped to both 'a' and 'r'.
+
+Example 3:
+Input: s = "paper", t = "title"
+Output: true'''
