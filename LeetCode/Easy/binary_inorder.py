0a17feebd2f6d79e8c56c9b22332b663ceaab762
Steven Itti Leon <154503819+stevenittileon@users.noreply.github.com>
2025-06-24 10:42:55 -0700

    Add files via upload

---
+++ LeetCode/Easy/binary_inorder.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def inorderTraversal(self, root):
+        res = []
+        self.help(root, res)
+        return res
+
+    def help(self, root, res):
+        if root:
+            self.help(root.left, res)
+            res.append(root.val)
+            self.help(root.right, res)
+
+'''94. Binary Tree Inorder Traversal
+
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+
+Example 2:
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+Output: [4,2,6,5,7,1,3,9,8]
+
+Example 3:
+Input: root = []
+Output: []
+
+Example 4:
+Input: root = [1]
+Output: [1]'''
