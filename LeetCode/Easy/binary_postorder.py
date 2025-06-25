e4eaba72d33f9b13beceeb7b7dd06f96f792f160
Steven Itti Leon <154503819+stevenittileon@users.noreply.github.com>
2025-06-25 10:10:24 -0700

    Add files via upload

---
+++ LeetCode/Easy/binary_postorder.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def postorderTraversal(self, root):
+        res = []
+        self.dfs_post(root, res)
+        return res
+
+    def dfs_post(self, root, res):
+        if root:
+            self.dfs_post(root.left, res)
+            self.dfs_post(root.right, res)
+            res.append(root.val)
+
+'''145. Binary Tree Postorder Traversal
+
+Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [3,2,1]
+
+Example 2:
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+Output: [4,6,7,5,2,9,8,3,1]
+
+Example 3:
+Input: root = []
+Output: []
+
+Example 4:
+Input: root = [1]
+Output: [1]'''
