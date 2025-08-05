3cbc5918978872f94325f98588934db571300ab7
Steven Itti Leon <154503819+stevenittileon@users.noreply.github.com>
2025-08-06 00:01:55 +0530

    Delete HackerRank SQL directory

--- HackerRank SQL/weather_observation_station_6.sql
+++
@@ -1,9 +0,0 @@
-
-SELECT DISTINCT CITY AS CIUDAD 
-FROM STATION 
-WHERE CITY REGEXP '^[aeiou]'
-
-/*
-Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. 
-Your result cannot contain duplicates.
-*/
