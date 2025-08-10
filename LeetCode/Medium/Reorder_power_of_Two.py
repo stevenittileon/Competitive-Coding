435aab97df307a7a287e00de33d26f0ada46329a
Steven Itti Leon <154503819+stevenittileon@users.noreply.github.com>
2025-08-10 14:11:40 +0530

    Add files via upload

---
+++ LeetCode/Medium/Reorder_power_of_Two.py
@@ -0,0 +1,21 @@
+class Solution:
+    def reorderedPowerOf2(self, n: int) -> bool:
+        target = ''.join(sorted(str(n)))
+        for i in range(31):
+            if "".join(sorted(str(2**i))) == target:
+                return True
+        return False
+
+'''869. Reordered Power of 2
+
+You are given an integer n. 
+We reorder the digits in any order (including the original order) such that the leading digit is not zero.
+Return true if and only if we can do this so that the resulting number is a power of two.
+
+Example 1:
+Input: n = 1
+Output: true
+
+Example 2:
+Input: n = 10
+Output: false'''
