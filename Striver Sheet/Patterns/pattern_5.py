558285d6ab973b3a6076f9d1394671b1073a44b0
Steven Itti Leon <154503819+stevenittileon@users.noreply.github.com>
2026-08-19 20:41:21 +0530

    Add files via upload

---
+++ Striver Sheet/Patterns/pattern_5.py
@@ -0,0 +1,5 @@
+N = int(input())
+for N in range(N,-1,-1):
+    for j in range(N):
+        print("*", end="")
+    print("\n")
---
+++ Striver Sheet/Patterns/pattern_6.py
@@ -0,0 +1,5 @@
+N = int(input())
+for N in range(N,-1,-1):
+    for j in range(N):
+        print(j+1, end="")
+    print("\n")
